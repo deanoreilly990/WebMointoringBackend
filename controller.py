53bcf0f3939434ca00338988a773b1c24a011dea
AWS Freelance Dev <aws.freelance.dev@gmail.com>
2018-11-05 16:36:45 +0000

    Importing and setup of Twilio

--- controller.py
+++ controller.py
@@ -1,10 +1,15 @@
 #process and construct the data into a JSON format
 # Wait for the API calls - send back JSON object --
 
+import logging
+from logging.config import fileConfig
+
 import backend
 
 
 def main():
+    fileConfig('logging_config.ini')
+    logger = logging.getLogger()
     backend.getLocalCPUlevels()
 
 
