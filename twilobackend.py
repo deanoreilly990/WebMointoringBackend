9f05edfb23f4e31e8b8ea6ea299ae717cc837c80
AWS Freelance Dev <aws.freelance.dev@gmail.com>
2018-11-05 17:37:52 +0000

    Logging Implemented Config.json used to store Keys and Variables Emergency Message successfully sent to outboundNumber

--- twilobackend.py
+++ twilobackend.py
@@ -1,14 +1,31 @@
 # Backend function used to communictate with the Twilio app
 # https://github.com/twilio/twilio-python
 
-from __future__ import print_function
 
 
 def constructClient():
-    print()
+    from controller import logger_Global
+    logger = logger_Global()
+    logger.info('Client contructed TWilio - 200')
     from twilio.rest import Client
-    account = ""
-    token = ""
+    import json
+    with open('config.json') as f:
+        data = json.load(f)
+    account = data['twilio']['accessKey']
+    token = data['twilio']['secretKey']
+    outboundNumber = data['twilio']['outboundNumber']
+    InboundNumber = data['twilio']['TwilioNumber']
     client = Client(account, token)
+    return client, outboundNumber, InboundNumber
 
-    return client
+
+def sendEmergencyMessage(EmegencyMessage):
+    # Function designed to send an emergency SMS to endpoint
+    from controller import logger_Global
+    logger = logger_Global()
+    logger.info('Emergency Message Invoked - Emergency In Place')
+    logger.info(EmegencyMessage)
+    client, outbound, TwilioNum = constructClient()
+    message = client.messages.create(to=outbound, from_=TwilioNum,
+                                     body=EmegencyMessage)
+    logger.info(message)
